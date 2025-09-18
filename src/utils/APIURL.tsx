83e0e27780b616f52c66153dc3217eab9072bed8
Triioade <trioade29@gmail.com>
2025-09-18 23:20:37 +0700

    tadi gagal anjg

--- src/utils/APIURL.tsx
+++ src/utils/APIURL.tsx
@@ -1,1 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+// src/utils/APIURL.tsx
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default API_URL;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,12 @@
+/// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/react" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // kalau nanti ada tambahan ENV, taruh di sini juga
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
