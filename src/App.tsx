fbca29e6d7237743a586fda77f7a76123b9b7ebd
Triioade <trioade29@gmail.com>
2025-09-18 20:54:06 +0700

    coba fixnya lwat path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./pages/AuthPages/SignIn";
 import SignUp from "./pages/AuthPages/SignUp";
 import NotFound from "./pages/OtherPage/NotFound";
@@ -6,52 +6,51 @@ import AppLayout from "./layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
 import TaskUserPage from "./pages/Task/Taskuser";
 import UpdateEmailPage from "./pages/email/email";
-import ProtectedRoute from "./protectroute"; 
-import HomeRedirect from "./pages/Home/home";
+import ProtectedRoute from "./protectroute";
 import { Toaster } from "react-hot-toast";
 import AbsenPage from "./pages/absen/absen";
 
-
 // ./gradlew assembleRelease
-
-
 
 export default function App() {
   return (
     <Router>
       <ScrollToTop />
-<Toaster
-  position="top-center"
-  reverseOrder={false}
-  toastOptions={{
-    style: {
-      marginTop: "80px", 
-    },
-  }}
-/>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            marginTop: "80px",
+          },
+        }}
+      />
 
       <Routes>
         {/* Public Routes */}
-         <Route path="/" element={<HomeRedirect />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
 
-
         {/* Protected Routes inside Dashboard Layout */}
         <Route element={<AppLayout />}>
+          {/* Task page jadi root "/" */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <TaskUserPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Support untuk user lama yang buka /task */}
+          <Route path="/task" element={<Navigate to="/" replace />} />
+
           <Route
             path="/absen"
             element={
               <ProtectedRoute>
                 <AbsenPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/task"
-            element={
-              <ProtectedRoute>
-                <TaskUserPage />
               </ProtectedRoute>
             }
           />
@@ -64,6 +63,7 @@ export default function App() {
             }
           />
 
+          {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
